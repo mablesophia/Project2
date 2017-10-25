d92784f4545291d41e8af4dff7d9b5f29d635b25
RayLee2727 <raylee27@foxmail.com>
2017-10-25 12:57:28 -0700

    rm.scan & rm.getAttribute

--- src/rm/rm.h
+++ src/rm/rm.h
@@ -1,9 +1,10 @@
-
 #ifndef _rm_h_
 #define _rm_h_
 
 #include <string>
 #include <vector>
+#include <cassert>
+#include <unordered_set>
 
 #include "../rbf/rbfm.h"
 
@@ -63,15 +64,27 @@ public:
       RM_ScanIterator &rm_ScanIterator);
 
 // Extra credit work (10 points)
-public:
-  RC addAttribute(const string &tableName, const Attribute &attr);
+  public:
+    RC addAttribute(const string &tableName, const Attribute &attr);
 
-  RC dropAttribute(const string &tableName, const string &attributeName);
-
+    RC dropAttribute(const string &tableName, const string &attributeName);
 
 protected:
   RelationManager();
   ~RelationManager();
+
+private:
+  static RelationManager *_rm;
+  RecordBasedFileManager *_rbfm;
+  FILE *stats;
+
+  vector<Attribute> vector_col();
+  vector<Attribute> vector_table();
+  void getTableAttr(vector<Attribute> &attrs);
+  void getColumnAttr(vector<Attribute> &attrs);
+
+  RC insert_table(FileHandle &fh, const int tableId, const string name, const string ending);
+  RC insert_col(FileHandle &fh, const int tableId, const string name, const int colType, const int colLength, const int colPos);
 
 };
 
